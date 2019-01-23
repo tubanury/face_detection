f09ac950b8917043e358668ab785c1fc3ec532c5
Tuba Nur Yıldız <yldztubanur12@gmail.com>
2019-01-23 11:15:13 +0300

    Add files via upload
    Detecting faces from an url of image using urllib

---
+++ detect_faces.py
@@ -0,0 +1,52 @@
+import numpy as np
+import argparse
+import cv2
+import urllib
+from urllib.request import urlopen
+
+
+ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", required=True, help="path the input image")
+ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+#image = cv2.imread(args["image"])
+req = urllib.request.urlopen('http://10.154.3.199:8000/cgi-bin/camera?resolution=320&page=1548066835550&Language=0')
+arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+image = cv2.imdecode(arr, -1)
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+
+for i in range(0, detections.shape[2]):
+    # extract the confidence (i.e., probability) associated with the
+    # prediction
+    confidence = detections[0, 0, i, 2]
+
+    # filter out weak detections by ensuring the `confidence` is
+    # greater than the minimum confidence
+    if confidence > args["confidence"]:
+        # compute the (x, y)-coordinates of the bounding box for the
+        # object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        # draw the bounding box of the face along with the associated
+        # probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(image, (startX, startY), (endX, endY),
+                      (0, 0, 255), 2)
+        cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
